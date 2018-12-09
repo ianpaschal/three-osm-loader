0b19f3fb00036ef4449f682b9e51475dca27eb61
Ian <projects@ianpaschal.com>
2018-12-09 20:52:29 +0100

    Begin "build" function for loader

--- src/core/OSMLoader.ts
+++ src/core/OSMLoader.ts
@@ -1,6 +1,9 @@
 import XML2JS from "xml2js";
 import FS from "fs";
 import cleanXML from "../utils/cleanXML";
+import calcCenter from "../utils/calcCenter";
+import GeoCoordinate from "./GeoCoordinate";
+import { Geometry, Line, LineBasicMaterial, Object3D } from "three";
 
 class OSMLoader {
 	load( path, onError, onLoad ) {
@@ -23,6 +26,8 @@ class OSMLoader {
 				// - Recursively parsing child arrays
 				const output = cleanXML( json.osm );
 
+				console.log( JSON.stringify( output, null, 4 ) );
+
 				if ( onLoad && typeof onLoad === "function" ) {
 					const result = this.build( output );
 					return onLoad( result );
@@ -32,6 +37,33 @@ class OSMLoader {
 	}
 	build( input ) {
 		// Build the geometry!
+
+		// Find the center
+		const center = calcCenter(
+			new GeoCoordinate( input.bounds[ 0 ].maxlat, input.bounds[ 0 ].maxlon ),
+			new GeoCoordinate( input.bounds[ 0 ].minlat, input.bounds[ 0 ].minlon )
+		);
+
+		const buildings = new Object3D();
+		// Collect ways with the "building" tag
+		input.way.forEach( ( way ) => {
+			if ( way.tags && way.tags.building ) {
+				const material = new LineBasicMaterial({
+					color: 0x0000ff
+				});
+				const geometry = new Geometry();
+				way.nd.forEach( ( ref ) => {
+					const data = input.node.find( ( node ) => {
+						return node.id === ref;
+					});
+					const coordinate = new GeoCoordinate( data.lat, data.lon );
+					geometry.vertices.push( coordinate.asCartesian( center ) );
+				});
+				geometry.vertices.push( geometry.vertices[ 0 ] );
+				buildings.add( new Line( geometry, material ) );
+			}
+		});
+		return buildings;
 	}
 }
 export default OSMLoader;
